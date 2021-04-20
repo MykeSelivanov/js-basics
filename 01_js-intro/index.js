85b518333b9bb58bf84d7c98b264e01cffddf4fd
MykeSelivanov <michael777selivanov@gmail.com>
2021-04-19 21:29:46 -0500

    fc

---
+++ 01_js-intro/index.js
@@ -0,0 +1,51 @@
+// console.log
+console.log(4 + 3);
+// alert(5 + 6);
+// document.write(7 + 9);
+
+// STATEMENTS -----------------------------
+console.log('Hi there'); // statement 1
+console.log('Cats are beautiful'); // statement 2
+console.log(12); // statement 3
+
+// VARIABLES ------------------------------
+var language = 'first -> JS';
+console.log(language);
+language = 'reassigning to - > Java';
+console.log(language);
+
+var sum = (4 + 3);
+console.log(sum);
+
+// declaration and initialization
+var box; // declare
+fcfsv = 2021;
+console.log(fcfsv);
+
+const age = 20;
+const year = 2020;
+console.log('this is age: ' + age + '\nthis is year: ' + year);
+
+// DATA TYPES --------------------------------
+// typeof - to check a type of any variable
+
+// a. Numbers
+var a = 10;
+var b = new Number(3);
+
+console.log(typeof a);
+console.log(typeof b);
+
+var summy = a + b; // addition
+var subby = a - b; // substraction
+var multy = a * b; // multiplication
+var divvy = a / b; // division
+var moddy = a % b; // modulus
+
+console.log('addition ', summy);
+console.log('substraction ', subby);
+console.log('multiplication ', multy);
+console.log('division', divvy);
+console.log('modulus', moddy);
+
+
