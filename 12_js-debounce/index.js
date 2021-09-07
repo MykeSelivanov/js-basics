5ada175aa8958798e55c3074ccd31aac7131a714
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-06 22:54:55 -0500

    debounced search input

--- 12_js-debounce/index.js
+++ 12_js-debounce/index.js
@@ -1,5 +1,25 @@
+const output = document.getElementById('output');
+const inputField = document.getElementById('input-id');
 
-document.getElementById('input-id').addEventListener('click', () => {
-    console.log('click');
-});
+const debounced = (func, event, delay) => {
+    let timeoutId;
+
+    return function(...args) {
+        if(timeoutId) {
+            clearTimeout(timeoutId);
+        }
+
+        setTimeout(() => {
+            func(...args);
+        }, delay);
+    }
+}
+
+const outputToPage = (event) => {
+    let inputText = event.target.value;
+    output.textContent = inputText;
+    // console.log('hello');
+}
+
+document.getElementById('input-id').addEventListener('keyup', debounced(outputToPage, event, 3000));
 
